773459b4bca9dc34f766501805542fb37ccb9856
sachingadge-eai <102816872+sachingadge-eai@users.noreply.github.com>
2023-11-22 03:23:54 +0530

    test

--- index.js
+++ index.js
@@ -1,53 +1,45 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
-const app = express()
+const app = express();
 const port = 3000;
 const host = '0.0.0.0';
 
-let finalPdf = async (link, name, res) => {
-    const browser = await puppeteer.launch({
-        headless: 'new', // Opt-in to the new headless mode
-        // Other configurations...
-    });
-    const page = await browser.newPage();
-    console.log('in final pdf----');
-    try {
-        console.log('in try----');
-        await page.goto(link, { waitUntil: 'networkidle0' });
-        console.log('page----');
-        const pdfBuffer = await page.pdf();
-        console.log(pdfBuffer, 'pdfBuffer----');
-        const pdfBase64 = pdfBuffer.toString('base64');
-        console.log(pdfBase64, 'pdfBase64----');
-        console.log("PDF Base64:", pdfBase64);
-        console.log(pdfBase64, 'pdfBase64----');
-        res.send(pdfBase64);
-    } catch (err) {
-        console.error('Error generating PDF:', err);
-        throw err;
-    } finally {
-        console.error('final:');
-        await browser.close();
-    }
-}
+const generatePDFFromHTML = async (htmlContent) => {
+  const browser = await puppeteer.launch({
+    headless: true, // Run Puppeteer in headless mode
+    // Other configurations...
+  });
+  const page = await browser.newPage();
+
+  try {
+    await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
+    const pdfBuffer = await page.pdf();
+    return pdfBuffer.toString('base64');
+  } catch (err) {
+    console.error('Error generating PDF:', err);
+    throw err;
+  } finally {
+    console.log('Closing browser...');
+    await browser.close();
+  }
+};
 
 app.get('/generatePDF', async (req, res) => {
-    if (req.headers.auth_token == 'EAI-PDF-Generate') {
-        // req.query.link = 'https://uat-e-portal.europ-assistance.in/admin/certificate-pdf?car_id=&prdID=0&subscription_id=3739227';
-        let url = decodeURIComponent(req.query.link);
-        console.log(url, "url");
-        try {
-            console.log(url, "in try");
-            const generate_res = await finalPdf(url, req.query.name, res);
-            console.log('generate res', generate_res);
-        } catch (error) {
-            console.log('error', error);
-            res.status(500).send('Internal Server Error');
-        }
-    } else {
-        console.log("access denied");
-        res.status(401).send("Access Denied");
+  if (req.headers.auth_token === 'EAI-PDF-Generate') {
+    const htmlContent = '<html><body><h1>Hello, PDF!</h1></body></html>'; // Replace this with your HTML content
+
+    try {
+      const pdfBase64 = await generatePDFFromHTML(htmlContent);
+      res.send(pdfBase64);
+    } catch (error) {
+      console.error('Internal Server Error:', error);
+      res.status(500).send('Internal Server Error');
     }
+  } else {
+    res.status(401).send('Access Denied');
+  }
 });
 
-app.listen(port, host, () => console.log(`PDF Generator app listening on port ${port}!`));
+app.listen(port, host, () => {
+  console.log(`PDF Generator app listening on port ${port}!`);
+});
