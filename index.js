4e456c1a1bcb3bed5a598d9fda7733138becefe4
sachingadge-eai <102816872+sachingadge-eai@users.noreply.github.com>
2023-11-22 03:52:58 +0530

    link final

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ const generatePDFFromHTML = async (htmlContent) => {
 app.get('/generatePDF', async (req, res) => {
   if (req.headers.auth_token === 'EAI-PDF-Generate') {
     // const htmlContent = '<html><body><h1>Hello, PDF!</h1></body></html>'; // Replace this with your HTML content
-    const htmlContent = req.query.link; // Replace this with your HTML content
+    const htmlContent = 'https://uat-e-portal.europ-assistance.in/admin/certificate-pdf?car_id=&prdID=0&subscription_id=3739227'; // Replace this with your HTML content
+    // const htmlContent = req.query.link; // Replace this with your HTML content
     console.log(req.query.link, 'url');
     try {
       const pdfBase64 = await generatePDFFromHTML(htmlContent);
