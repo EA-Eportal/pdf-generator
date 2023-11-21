08698a821946bfb573e44d91726af65bfe05161e
sachingadge-eai <102816872+sachingadge-eai@users.noreply.github.com>
2023-11-22 01:57:49 +0530

    puppeteer

--- index.js
+++ index.js
@@ -1,58 +1,40 @@
 const express = require('express');
-var fs = require('fs')
-var conversion = require("phantom-html-to-pdf")();
+const puppeteer = require('puppeteer');
 const app = express()
 const port = 3000;
 const host = '0.0.0.0';
-
-let finalPdf = (link, name, res) => {
-    //Replace {html: ...} to {url : link}
-    conversion({ url: link }, function (err, pdfStream) {
-        if (err) {
-            throw err;
-        } else {
-            var chunks = [];
-
-            pdfStream.stream.on('data', (chunk) => {
-                // Collect PDF data in chunks
-                chunks.push(chunk);
-            });
-
-
-            pdfStream.stream.on('end', () => {
-                var pdfBuffer = Buffer.concat(chunks);
-                var pdfBase64 = pdfBuffer.toString('base64');
-                console.log("PDF Base64:", pdfBase64);
-                // var decodedData = Buffer.from(pdfBase64, 'base64');
-                // var outputFilePath = `${__dirname}/to/${name}.pdf`;
-                // fs.writeFile(outputFilePath, decodedData, 'binary', (err) => {
-                //     if (err) {
-                //         console.error('Error writing PDF file:', err);
-                //     } else {
-                //         console.log('PDF file saved:', outputFilePath);
-                //     }
-                // });
-
-                res.send(pdfBase64);
-            });
-        }
-    });
+ 
+let finalPdf = async (link, name, res) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+ 
+    try {
+        await page.goto(link, { waitUntil: 'networkidle0' });
+        const pdfBuffer = await page.pdf();
+        const pdfBase64 = pdfBuffer.toString('base64');
+        console.log("PDF Base64:", pdfBase64);
+        res.send(pdfBase64);
+    } catch (err) {
+        console.error('Error generating PDF:', err);
+        throw err;
+    } finally {
+        await browser.close();
+    }
 }
-
-
-app.get('/generatePDF', (req, res) => {
+ 
+app.get('/generatePDF', async (req, res) => {
     if (req.headers.auth_token == 'EAI-PDF-Generate') {
-
         let url = decodeURIComponent(req.query.link);
         console.log(url, "url");
-        finalPdf(url, req.query.name, res);
-
+        try {
+            await finalPdf(url, req.query.name, res);
+        } catch (error) {
+            res.status(500).send('Internal Server Error');
+        }
     } else {
         console.log("access denied");
         res.status(401).send("Access Denied");
     }
 });
-
-
-
-app.listen(port, host, () => console.log(`Example app listening on port ${port}!`));
+ 
+app.listen(port, host, () => console.log(`PDF Generator app listening on port ${port}!`));
